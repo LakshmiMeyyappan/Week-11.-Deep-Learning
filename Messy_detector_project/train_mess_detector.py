47180e3e7d9cb00d3ffdd150c0a6fe7704950790
Lakshmi Meyyappan <lakshmikec91@gmail.com>
2025-07-18 10:28:00 +0530

    Add files via upload

--- Messy_detector_project/train_mess_detector.py
+++ Messy_detector_project/train_mess_detector.py
@@ -1,1 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 16 22:28:32 2025
 
+@author: admin
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from imutils import paths
+
+INIT_LR = 1e-4
+EPOCHS = 10
+BS = 32
+IMAGE_SIZE = (224, 224)
+DATASET_PATH = "dataset"  
+
+print("[INFO] Loading images...")
+imagePaths = list(paths.list_images(DATASET_PATH))
+data = []
+labels = []
+
+for path in imagePaths:
+    label = path.split(os.path.sep)[-2]
+    image = load_img(path, target_size=IMAGE_SIZE)
+    image = img_to_array(image)
+    image = preprocess_input(image)
+    data.append(image)
+    labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
+
+aug = ImageDataGenerator(
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
+
+baseModel = MobileNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+    layer.trainable = False
+
+
+print("[INFO] Compiling model...")
+opt = Adam(learning_rate=INIT_LR)
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+print("[INFO] Training model...")
+H = model.fit(
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
+
+model.save("messy_clean_detector.h5")
+
+plt.plot(H.history["loss"], label="train_loss")
+plt.plot(H.history["val_loss"], label="val_loss")
+plt.plot(H.history["accuracy"], label="train_acc")
+plt.plot(H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
